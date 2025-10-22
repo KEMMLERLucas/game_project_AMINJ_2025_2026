6b266b23aa1f61514d2075bd3070645c4663db02
Tato-Sauruus <nq8wxtyt7v@privaterelay.appleid.com>
2025-10-22 09:18:19 +0200

    Changes
    - Changement du nom de la scene
    - Nouveau script vide pour comportement du perso

---
+++ Assets/Scripts/CharacterBehaviourScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CharacterBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
