3521e1ccd16e61a84eda1e313c0b7286bda0ec46
aymericdefonclare-hub <aymericdefonclare@gmail.com>
2025-11-14 09:39:05 +0100

    modified cat prefab to change sprite depending of movement

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -5,18 +5,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float collisionOffset;
+    public Sprite spriteFront;
+    public Sprite spriteBack;
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
+    SpriteRenderer spriteRenderer;
     Transform cornerNETransform;
     Transform cornerNWTransform;
     Transform cornerSETransform;
     Transform cornerSWTransform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Awake()
     {
         cornerNETransform = GameObject.Find("CornerNE").transform;
         cornerNWTransform = GameObject.Find("CornerNW").transform;
         cornerSETransform = GameObject.Find("CornerSE").transform;
         cornerSWTransform = GameObject.Find("CornerSW").transform;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,18 +32,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < cornerNETransform.position.x - collisionOffset && transform.position.x < cornerSETransform.position.x - collisionOffset)
         {
             transform.position += Vector3.right * speed;
+            spriteRenderer.sprite = spriteRight;
         }
         if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > cornerNWTransform.position.x + collisionOffset && transform.position.x > cornerSWTransform.position.x + collisionOffset)
         {
             transform.position += Vector3.left * speed;
+            spriteRenderer.sprite = spriteLeft;
         }
         if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < cornerNETransform.position.y - collisionOffset && transform.position.y < cornerNWTransform.position.y - collisionOffset)
         {
             transform.position += Vector3.up * speed;
+            spriteRenderer.sprite = spriteBack;
         }
         if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > cornerSETransform.position.y + collisionOffset && transform.position.y > cornerSWTransform.position.y + collisionOffset)
         {
             transform.position += Vector3.down * speed;
+            spriteRenderer.sprite = spriteFront;
         }
     }
 
