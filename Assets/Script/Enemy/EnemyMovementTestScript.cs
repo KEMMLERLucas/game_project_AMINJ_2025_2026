9b4f0a01f31cb20da0cd4dbd9cdf352cf8f18efd
KEMMLER Lucas <kemmler2u@etu.univ-lorraine.fr>
2025-11-19 13:34:52 +0100

    Temporary test for enemy movement

---
+++ Assets/Script/Enemy/EnemyMovementTestScript.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementTestScript : MonoBehaviour
+{
+    private Transform targetPos;
+    public float detectionRange = 10f;
+    public bool isInRange = false;
+    NavMeshAgent agent;
+    public LayerMask playerLayer;
+    private Rigidbody2D rb;
+     public enum MovementType
+    {
+        Linear,
+        LinearSlowing,
+        PathFindingLinear,
+        PathFindingLinearSlowing
+    }
+
+    public MovementType movementType = MovementType.LinearSlowing;
+    public float movementSpeed = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetPos = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPlayerInRange();
+        if (isInRange)
+        {
+            switch (movementType){
+            case MovementType.Linear :
+                //todo
+                break;
+            case MovementType.LinearSlowing :
+                rb.linearVelocity = Vector2.zero;
+                rb.linearVelocity = new Vector2(
+                targetPos.position.x - gameObject.transform.position.x,
+                targetPos.position.y - gameObject.transform.position.y
+                ) * movementSpeed;
+                break;
+            case MovementType.PathFindingLinear : 
+                agent.SetDestination(targetPos.position);
+                //todo
+                break;
+            case MovementType.PathFindingLinearSlowing :
+                //todo
+                break;
+ 
+            }
+        }
+        else
+        {
+            //Add wandering function
+        }
+        
+    }
+    void CheckPlayerInRange()
+    {
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(transform.position, detectionRange, playerLayer);
+        if (hitPlayers.Length > 0)
+        {
+            isInRange = true;
+        }
+    }
+}
